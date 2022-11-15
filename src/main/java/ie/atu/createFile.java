3b79e79a7bde5a0801c6c0bcebd4e02bfabc9575
G00402034 <G00402034@atu.ie>
2022-11-15 15:17:38 +0000

    created new Printwriter class

--- src/main/java/ie/atu/createFile.java
+++ src/main/java/ie/atu/createFile.java
@@ -1,17 +1,21 @@
 package ie.atu;
 
 import java.io.*;
+import java.util.Scanner;
 
 public class createFile {
     public static void main(String[] args) {
 
         {
-           File myFile = new File("MyFile.txt");
-           System.out.println("My file is located at " + myFile.getAbsolutePath());
-           String words = "writing words";
+
+
             try
             {
-              FileWriter myFileWrite = new FileWriter(myFile,true);
+                Scanner keyboard = new Scanner(System.in);
+                System.out.println("Please enter a file name:");
+                String filename = keyboard.nextLine();
+                Scanner inputFile = new Scanner (new File(filename));
+              FileWriter myFileWrite = new FileWriter(filename,true);
               myFileWrite.write("add new words");
               myFileWrite.close();
             }catch(IOException e)
