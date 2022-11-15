c469accc6dee8a11776bb627fca85b76296c8920
G00402034 <G00402034@atu.ie>
2022-11-15 14:13:24 +0000

    created new FileWriter object

--- src/main/java/ie/atu/createFile.java
+++ src/main/java/ie/atu/createFile.java
@@ -8,6 +8,17 @@ public class createFile {
         {
            File myFile = new File("MyFile.txt");
            System.out.println("My file is located at " + myFile.getAbsolutePath());
+           String words = "writing words";
+            try
+            {
+              FileWriter myFileWrite = new FileWriter(myFile,true);
+              myFileWrite.write("add new words");
+              myFileWrite.close();
+            }catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+
         }
     }
 
